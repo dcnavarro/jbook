fbd1d7a87a8ca60e72f17c275a76c34d4e68b9fb
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-06-15 16:52:11 -0300

    code editor and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () =>{
     const ref = useRef<any>(); 
@@ -66,6 +67,11 @@ const App = () =>{
 
     return(
         <div>
+            <CodeEditor
+                initialValue="const a = 1;"
+                onChange={(value) => setInput(value)}
+            
+            />
             <textarea  value={input} onChange={event=> setInput(event.target.value)}></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
